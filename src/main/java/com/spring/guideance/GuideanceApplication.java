387cb75002aa3f0f1b5f20f986856648e069bb49
CHAE <chaso1495@naver.com>
2023-07-19 00:16:54 +0900

    [init] first commit

---
+++ src/main/java/com/spring/guideance/GuideanceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.guideance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideanceApplication.class, args);
+	}
+
+}
