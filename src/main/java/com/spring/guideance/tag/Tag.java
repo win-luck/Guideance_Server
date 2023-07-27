cf82299695025def07d8ac2aebf5b836fe73abad
CHAE <chaso1495@naver.com>
2023-07-27 22:58:48 +0900

    [feat] #5 기존 domain 일부 수정

--- src/main/java/com/spring/guideance/tag/Tag.java
+++ src/main/java/com/spring/guideance/tag/Tag.java
@@ -1,4 +1,26 @@
 package com.spring.guideance.tag;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
 public class Tag {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tagName;
+
+    private String tagDescription;
+
+    @OneToMany(mappedBy = "tag") // 이 태그를 구독하는 유저들
+    private List<UserTag> userTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "tag") // 이 태그를 가지고 있는 게시글들
+    private List<ArticleTag> articleTags = new ArrayList<>();
 }
--- src/main/java/com/spring/guideance/user/User.java
+++ src/main/java/com/spring/guideance/user/User.java
@@ -1,13 +1,17 @@
 package com.spring.guideance.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.spring.guideance.article.Article;
+import com.spring.guideance.comment.Comment;
+import com.spring.guideance.like.Likes;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 // @RedisHash(value = "user", timeToLive = 100) // Redis에 저장될 객체임을 명시, timeToLive는 100초 동안 Redis에 저장됨을 의미
 @Entity
@@ -26,5 +30,19 @@ public class User {
     @JsonIgnore
     private String password; // 비밀번호
 
-    private int level; // 유저의 글쓰기 레벨
+    private int level; // 유저의 레벨
+
+    private int exp; // 유저의 경험치
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Article> articles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Likes> likes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Notice> likeArticles = new ArrayList<>();
 }
