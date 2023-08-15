f9cb75e186fba2667fdfd39a92df9508bf8d6c1a
CHAE <chaso1495@naver.com>
2023-08-15 19:21:47 +0900

    [feat] #17 UserController 구현

--- src/main/java/com/spring/guideance/user/controller/UserController.java
+++ src/main/java/com/spring/guideance/user/controller/UserController.java
@@ -1,4 +1,87 @@
 package com.spring.guideance.user.controller;
 
+import com.spring.guideance.post.dto.response.ResponseSimpleArticleDto;
+import com.spring.guideance.tag.dto.ResponseTagDto;
+import com.spring.guideance.user.dto.request.CreateUserDto;
+import com.spring.guideance.user.dto.request.LoginUserDto;
+import com.spring.guideance.user.dto.request.UpdateUserDto;
+import com.spring.guideance.user.dto.response.ResponseNoticeDto;
+import com.spring.guideance.user.dto.response.ResponseUserDto;
+import com.spring.guideance.user.service.UserService;
+import com.spring.guideance.util.api.ApiResponse;
+import com.spring.guideance.util.exception.ResponseCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    // 회원가입
+    @PostMapping("/create")
+    public ApiResponse<Long> createUser(@RequestBody CreateUserDto createUserDto) {
+        return ApiResponse.success(userService.createUser(createUserDto), ResponseCode.USER_CREATED.getMessage());
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public ApiResponse<ResponseUserDto> login(@RequestBody LoginUserDto loginUserDto) {
+        return ApiResponse.success(userService.login(loginUserDto.getEmail()), ResponseCode.USER_LOGINED.getMessage());
+    }
+
+    // 회원정보 조회
+    @GetMapping("/{userId}")
+    public ApiResponse<ResponseUserDto> getUser(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUser(userId), ResponseCode.USER_FOUND.getMessage());
+    }
+
+    // 회원정보 수정
+    @PutMapping("/{userId}/update")
+    public ApiResponse<Void> updateUser(@PathVariable Long userId, @RequestBody UpdateUserDto updateUserDto) {
+        // 이미지 업로드 관련 S3 환경 추후 추가
+        userService.updateUser(updateUserDto, null);
+        return ApiResponse.success(null, ResponseCode.USER_UPDATED.getMessage());
+    }
+
+    // 회원정보 삭제
+    @DeleteMapping("/{userId}/delete")
+    public ApiResponse<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ApiResponse.success(null, ResponseCode.USER_DELETED.getMessage());
+    }
+
+    // 작성한 게시물 조회
+    @GetMapping("/{userId}/articles/writes")
+    public ApiResponse<List<ResponseSimpleArticleDto>> getArticles(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUserArticles(userId), ResponseCode.ARTICLE_FOUND.getMessage());
+    }
+
+    // 구독한 태그 조회
+    @GetMapping("/{userId}/tags")
+    public ApiResponse<List<ResponseTagDto>> getTags(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUserTags(userId), ResponseCode.TAG_FOUND.getMessage());
+    }
+
+    // 수신한 알림 조회
+    @GetMapping("/{userId}/notices")
+    public ApiResponse<List<ResponseNoticeDto>> getNotices(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUserNotices(userId), ResponseCode.NOTICE_FOUND.getMessage());
+    }
+
+    // 좋아요 누른 게시물 조회
+    @GetMapping("/{userId}/articles/likes")
+    public ApiResponse<List<ResponseSimpleArticleDto>> getLikes(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUserLikesArticles(userId), ResponseCode.ARTICLE_FOUND.getMessage());
+    }
+
+    // 댓글 쓴 게시물 조회
+    @GetMapping("/{userId}/articles/comments")
+    public ApiResponse<List<ResponseSimpleArticleDto>> getComments(@PathVariable Long userId) {
+        return ApiResponse.success(userService.getUserCommentsArticles(userId), ResponseCode.ARTICLE_FOUND.getMessage());
+    }
 }
