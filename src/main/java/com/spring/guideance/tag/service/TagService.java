53605dfca1a9c4b8297f5464d497dde855148aad
CHAE <chaso1495@naver.com>
2023-08-15 15:21:09 +0900

    [exception] #15 TagService Exception 반영 및 stream 도입

--- src/main/java/com/spring/guideance/tag/service/TagService.java
+++ src/main/java/com/spring/guideance/tag/service/TagService.java
@@ -13,13 +13,17 @@ import com.spring.guideance.tag.repository.UserTagRepository;
 import com.spring.guideance.user.domain.User;
 import com.spring.guideance.user.dto.response.ResponseUserDto;
 import com.spring.guideance.user.repository.UserRepository;
+import com.spring.guideance.util.exception.ResponseCode;
+import com.spring.guideance.util.exception.ArticleException;
+import com.spring.guideance.util.exception.TagException;
+import com.spring.guideance.util.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,13 +39,13 @@ public class TagService {
     @Transactional
     public void addTagToArticle(Long articleId, List<String> tagList) {
         Article article = articleRepository.findById(articleId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 게시물입니다."));
+                .orElseThrow(() -> new ArticleException(ResponseCode.ARTICLE_NOT_FOUND));
 
         tagList.forEach(tagName -> {
             // 태그가 존재하지 않으면 새로운 태그를 생성하고, 게시물에 태그를 추가
             if (tagRepository.findByTagName(tagName).isEmpty()) {
                 Long tagId = createTag(tagName);
-                articleTagRepository.save(ArticleTag.createArticleTag(article, tagRepository.findById(tagId).orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."))));
+                articleTagRepository.save(ArticleTag.createArticleTag(article, tagRepository.findById(tagId).orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND))));
             } else {
                 // 이미 태그가 존재하면 게시물에 태그를 추가
                 Tag tag = tagRepository.findByTagName(tagName).get();
@@ -54,10 +58,11 @@ public class TagService {
     @Transactional
     public void subscribeTag(Long userId, Long tagId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 유저입니다."));
+                .orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
         Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."));
-        if(userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isPresent()) throw new IllegalStateException("이미 구독한 태그입니다.");
+                .orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND));
+        if (userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isPresent())
+            throw new TagException(ResponseCode.TAG_ALREADY_SUBSCRIBED);
         userTagRepository.save(UserTag.createUserTag(tag, user));
     }
 
@@ -65,10 +70,11 @@ public class TagService {
     @Transactional
     public void unsubscribeTag(Long userId, Long tagId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 유저입니다."));
+                .orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
         Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."));
-        if(userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isEmpty()) throw new IllegalStateException("구독하지 않은 태그입니다.");
+                .orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND));
+        if (userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isEmpty())
+            throw new TagException(ResponseCode.UNSUBSCRIBE_TAG);
         Long id = userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).get().getId();
         userTagRepository.deleteById(id);
     }
@@ -84,7 +90,7 @@ public class TagService {
     private void validateDuplicateTag(String tagName) {
         tagRepository.findByTagName(tagName)
                 .ifPresent(tag -> {
-                    throw new IllegalStateException("이미 존재하는 태그입니다.");
+                    throw new TagException(ResponseCode.TAG_ALREADY_EXISTS);
                 });
     }
 
@@ -92,97 +98,83 @@ public class TagService {
     @Transactional
     public void deleteTag(Long tagId) {
         Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."));
+                .orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND));
         tagRepository.delete(tag);
     }
 
     // 태그 목록 조회 (태그명, 게시물수, 구독자수) - 유저가 구독한 태그가 앞에 오도록, 가장 최신으로 구독한 태그가 앞에 오도록 정렬
     public List<ResponseTagDto> getTagList(Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 유저입니다."));
+                .orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
         List<Tag> tagList = tagRepository.findAll();
-        List<ResponseTagDto> responseTagList = new ArrayList<>();
 
-        for (Tag tag : tagList) {
-            boolean isSubscribed = userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isPresent();
-            int subscribeCount = tag.getUserTags().size(); // 구독자 수
-            int likeCount = tag.getArticleTags().size(); // 게시물 수
+        return tagList.stream()
+                .map(tag -> {
+                    boolean isSubscribed = userTagRepository.findByTagIdAndUserId(tag.getId(), user.getId()).isPresent();
+                    int subscribeCount = tag.getUserTags().size(); // 구독자 수
+                    int likeCount = tag.getArticleTags().size(); // 게시물 수
 
-            ResponseTagDto responseTagDto = new ResponseTagDto(tag.getId(), tag.getTagName(), subscribeCount, likeCount);
-            responseTagDto.setSubscribed(isSubscribed);
-            responseTagList.add(responseTagDto);
-        }
-
-        responseTagList.sort((tag1, tag2) -> {
-            if (tag1.isSubscribed() && !tag2.isSubscribed()) return -1;
-            else if (!tag1.isSubscribed() && tag2.isSubscribed()) return 1;
-            else {
-                if (tag1.isSubscribed()) {
-                    Instant createdAt1 = Instant.from(userTagRepository.findByTagIdAndUserId(tag1.getTagId(), userId)
-                            .orElseThrow(IllegalStateException::new)
-                            .getCreatedAt());
-                    Instant createdAt2 = Instant.from(userTagRepository.findByTagIdAndUserId(tag2.getTagId(), userId)
-                            .orElseThrow(IllegalStateException::new)
-                            .getCreatedAt());
-                    return createdAt2.compareTo(createdAt1);
-                } else {
-                    return 0;
-                }
-            }
-        });
-        return responseTagList;
+                    ResponseTagDto responseTagDto = new ResponseTagDto(tag.getId(), tag.getTagName(), subscribeCount, likeCount);
+                    responseTagDto.setSubscribed(isSubscribed);
+                    return responseTagDto;
+                }).sorted((tag1, tag2) -> {
+                    if (tag1.isSubscribed() && !tag2.isSubscribed()) return -1;
+                    else if (!tag1.isSubscribed() && tag2.isSubscribed()) return 1;
+                    else {
+                        if (tag1.isSubscribed()) {
+                            Instant createdAt1 = Instant.from(userTagRepository.findByTagIdAndUserId(tag1.getTagId(), userId)
+                                    .orElseThrow(IllegalStateException::new)
+                                    .getCreatedAt());
+                            Instant createdAt2 = Instant.from(userTagRepository.findByTagIdAndUserId(tag2.getTagId(), userId)
+                                    .orElseThrow(IllegalStateException::new)
+                                    .getCreatedAt());
+                            return createdAt2.compareTo(createdAt1);
+                        } else {
+                            return 0;
+                        }
+                    }
+                }).collect(Collectors.toList());
     }
 
     // 태그 검색
     public List<ResponseTagDto> searchTag(String tagName) {
         List<Tag> tagList = tagRepository.findByTagNameContaining(tagName);
-        List<ResponseTagDto> responseTagList = new ArrayList<>();
 
-        for (Tag tag : tagList) {
-            int subscribeCount = tag.getUserTags().size(); // 구독자 수
-            int likeCount = tag.getArticleTags().size(); // 게시물 수
-
-            ResponseTagDto responseTagDto = new ResponseTagDto(tag.getId(), tag.getTagName(), subscribeCount, likeCount);
-            responseTagList.add(responseTagDto);
-        }
-
-        return responseTagList;
+        return tagList.stream()
+                .map(tag -> new ResponseTagDto(tag.getId(), tag.getTagName(), tag.getUserTags().size(), tag.getArticleTags().size()))
+                .collect(Collectors.toList());
     }
 
     // 특정 태그가 포함된 게시물 목록 조회
     public List<ResponseSimpleArticleDto> getArticleListByTag(Long tagId) {
         Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."));
+                .orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND));
         List<ArticleTag> articleTagList = articleTagRepository.findByTagId(tag.getId());
-        List<ResponseSimpleArticleDto> responseArticleList = new ArrayList<>();
 
-        for (ArticleTag articleTag : articleTagList) {
-            Article article = articleTag.getArticle();
-            User user = article.getUser();
-            int likeCount = article.getArticleTags().size(); // 좋아요 수
-            int commentCount = article.getComments().size(); // 댓글 수
-            ResponseSimpleArticleDto responseSimpleArticleDto = new ResponseSimpleArticleDto(article.getId(), article.getTitle(), article.getContents(), user.getName(), likeCount, commentCount);
-            responseArticleList.add(responseSimpleArticleDto);
-        }
-
-        return responseArticleList;
+        return articleTagList.stream()
+                .map(articleTag -> {
+                    Article article = articleTag.getArticle();
+                    User user = article.getUser();
+                    int likeCount = article.getArticleTags().size(); // 좋아요 수
+                    int commentCount = article.getComments().size(); // 댓글 수
+                    return new ResponseSimpleArticleDto(article.getId(), article.getTitle(), article.getContents(), user.getName(), likeCount, commentCount);
+                })
+                .collect(Collectors.toList());
     }
 
 
     // 특정 태그를 구독하는 유저 목록 조회
     public List<ResponseUserDto> getUserListByTag(Long tagId) {
         Tag tag = tagRepository.findById(tagId)
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 태그입니다."));
+                .orElseThrow(() -> new TagException(ResponseCode.TAG_NOT_FOUND));
         List<UserTag> userTagList = userTagRepository.findByTagId(tag.getId());
-        List<ResponseUserDto> responseUserList = new ArrayList<>();
 
-        for (UserTag userTag : userTagList) {
-            User user = userTag.getUser();
-            ResponseUserDto responseUserDto = new ResponseUserDto(user);
-            responseUserList.add(responseUserDto);
-        }
-
-        return responseUserList;
+        return userTagList.stream()
+                .map(userTag -> {
+                    User user = userTag.getUser();
+                    return new ResponseUserDto(user);
+                })
+                .collect(Collectors.toList());
     }
 
 
