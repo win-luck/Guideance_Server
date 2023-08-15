2c4527e70bba21f0e753d20b91aafc1467aad58c
CHAE <chaso1495@naver.com>
2023-08-15 15:19:12 +0900

    [exception] #15 ResponseCode 보강

--- src/main/java/com/spring/guideance/util/exception/ResponseCode.java
+++ src/main/java/com/spring/guideance/util/exception/ResponseCode.java
@@ -11,6 +11,7 @@ public enum ResponseCode {
 
     // 400 Bad Request
     BAD_REQUEST(HttpStatus.BAD_REQUEST, false, "잘못된 요청입니다."),
+    UNSUBSCRIBE_TAG(HttpStatus.BAD_REQUEST, false, "구독하지 않은 태그입니다."),
 
     // 404 Not Found
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "사용자를 찾을 수 없습니다."),
@@ -18,15 +19,52 @@ public enum ResponseCode {
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, false, "게시글을 찾을 수 없습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, false, "댓글을 찾을 수 없습니다."),
     LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, false, "좋아요를 찾을 수 없습니다."),
+    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, false, "태그를 찾을 수 없습니다."),
 
     // 405 Method Not Allowed
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, false, "권한이 없습니다."),
 
     // 409 Conflict
+    TAG_ALREADY_EXISTS(HttpStatus.CONFLICT, false, "이미 존재하는 태그입니다."),
+    TAG_ALREADY_SUBSCRIBED(HttpStatus.CONFLICT, false, "이미 구독한 태그입니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, false, "이미 존재하는 사용자입니다."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버에 오류가 발생하였습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버에 오류가 발생하였습니다."),
+
+    // 200 OK
+    USER_LOGINED(HttpStatus.OK, true, "로그인 되었습니다."),
+    TAG_UNSUBSCRIBED(HttpStatus.CREATED, true, "태그 구독이 취소되었습니다."),
+    USER_UPDATED(HttpStatus.CREATED, true, "사용자 정보가 수정되었습니다."),
+    ARTICLE_UPDATED(HttpStatus.CREATED, true, "게시글이 수정되었습니다."),
+    COMMENT_UPDATED(HttpStatus.CREATED, true, "댓글이 수정되었습니다."),
+    TAG_UPDATED(HttpStatus.CREATED, true, "태그가 수정되었습니다."),
+
+    // 조회
+    USER_FOUND(HttpStatus.OK, true, "사용자를 조회하였습니다."),
+    TAG_FOUND(HttpStatus.OK, true, "태그를 조회하였습니다."),
+    ARTICLE_FOUND(HttpStatus.OK, true, "게시글을 조회하였습니다."),
+    COMMENT_FOUND(HttpStatus.OK, true, "댓글을 조회하였습니다."),
+    LIKE_FOUND(HttpStatus.OK, true, "좋아요를 조회하였습니다."),
+    NOTICE_FOUND(HttpStatus.OK, true, "알림을 조회하였습니다."),
+
+    // 201 Created
+    USER_CREATED(HttpStatus.CREATED, true, "사용자가 생성되었습니다."),
+    TAG_CREATED(HttpStatus.CREATED, true, "태그가 생성되었습니다."),
+    ARTICLE_CREATED(HttpStatus.CREATED, true, "게시글이 생성되었습니다."),
+    COMMENT_CREATED(HttpStatus.CREATED, true, "댓글이 생성되었습니다."),
+    LIKE_CREATED(HttpStatus.CREATED, true, "좋아요가 생성되었습니다."),
+    NOTICE_CREATED(HttpStatus.CREATED, true, "알림이 생성되었습니다."),
+    TAG_SUBSCRIBED(HttpStatus.CREATED, true, "태그 구독이 완료되었습니다."),
+
+    // 204 No Content
+    ARTICLE_DELETED(HttpStatus.NO_CONTENT, true, "게시글이 삭제되었습니다."),
+    COMMENT_DELETED(HttpStatus.NO_CONTENT, true, "댓글이 삭제되었습니다."),
+    LIKE_DELETED(HttpStatus.NO_CONTENT, true, "좋아요가 취소되었습니다."),
+    NOTICE_DELETED(HttpStatus.NO_CONTENT, true, "알림이 삭제되었습니다."),
+    TAG_DELETED(HttpStatus.NO_CONTENT, true, "태그가 삭제되었습니다."),
+    USER_DELETED(HttpStatus.NO_CONTENT, true, "사용자 정보가 삭제되었습니다."),
+    USER_LOGOUT(HttpStatus.NO_CONTENT, true, "사용자가 로그아웃 되었습니다.");
 
     private final HttpStatus httpStatus;
     private final Boolean success;
