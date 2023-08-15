be8a5160c004e7ae1567060817befb6c1cbb7f5f
CHAE <chaso1495@naver.com>
2023-08-15 19:20:21 +0900

    [feat] #17 User별 주요 데이터 조회 기능 추가

--- src/main/java/com/spring/guideance/user/service/UserService.java
+++ src/main/java/com/spring/guideance/user/service/UserService.java
@@ -1,9 +1,11 @@
 package com.spring.guideance.user.service;
 
-import com.spring.guideance.tag.repository.UserTagRepository;
+import com.spring.guideance.post.dto.response.ResponseSimpleArticleDto;
+import com.spring.guideance.tag.dto.ResponseTagDto;
 import com.spring.guideance.user.domain.User;
 import com.spring.guideance.user.dto.request.CreateUserDto;
 import com.spring.guideance.user.dto.request.UpdateUserDto;
+import com.spring.guideance.user.dto.response.ResponseNoticeDto;
 import com.spring.guideance.user.dto.response.ResponseUserDto;
 import com.spring.guideance.user.repository.UserRepository;
 import com.spring.guideance.util.exception.ResponseCode;
@@ -12,13 +14,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserTagRepository userTagRepository;
 
     // 회원가입
     @Transactional
@@ -48,18 +51,85 @@ public class UserService {
         return new ResponseUserDto(user);
     }
 
-    // 회원정보 수정
+    // 회원정보 수정(이름/프사 변경)
     @Transactional
-    public void updateUser(UpdateUserDto updateUserDto) {
+    public void updateUser(UpdateUserDto updateUserDto, String imageUrl) {
         User user = userRepository.findById(updateUserDto.getUserId()).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
-        user.updateUser(updateUserDto.getUserName());
+        user.updateUser(updateUserDto.getUserName(), imageUrl);
         userRepository.save(user);
     }
 
     // 회원정보 삭제
     @Transactional
-    public void deleteUser(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
         userRepository.deleteById(user.getId());
     }
+
+    // 유저가 작성한 게시물 조회
+    public List<ResponseSimpleArticleDto> getUserArticles(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        return user.getArticles().stream()
+                .map(article -> new ResponseSimpleArticleDto(
+                        article.getId(),
+                        article.getTitle(),
+                        article.getContents(),
+                        article.getUser().getName(),
+                        article.getLikes().size(),
+                        article.getComments().size()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    // 유저가 구독한 태그 조회
+    public List<ResponseTagDto> getUserTags(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        return user.getUserTags().stream()
+                .map(userTag -> new ResponseTagDto(
+                        userTag.getTag().getId(),
+                        userTag.getTag().getTagName(),
+                        userTag.getTag().getTotalLikeCount(),
+                        userTag.getTag().getArticleTags().size()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    // 유저가 수신한 알림 조회
+    public List<ResponseNoticeDto> getUserNotices(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        return user.getUserNotices().stream()
+                .map(ResponseNoticeDto::new)
+                .collect(Collectors.toList());
+    }
+
+    // 유저가 좋아요 누른 게시물 조회
+    public List<ResponseSimpleArticleDto> getUserLikesArticles(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        return user.getLikes().stream()
+                .map(articleLike -> new ResponseSimpleArticleDto(
+                        articleLike.getArticle().getId(),
+                        articleLike.getArticle().getTitle(),
+                        articleLike.getArticle().getContents(),
+                        articleLike.getArticle().getUser().getName(),
+                        articleLike.getArticle().getLikes().size(),
+                        articleLike.getArticle().getComments().size()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    // 유저가 댓글 남긴 게시물 조회
+    public List<ResponseSimpleArticleDto> getUserCommentsArticles(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        return user.getComments().stream()
+                .map(comment -> new ResponseSimpleArticleDto(
+                        comment.getArticle().getId(),
+                        comment.getArticle().getTitle(),
+                        comment.getArticle().getContents(),
+                        comment.getArticle().getUser().getName(),
+                        comment.getArticle().getLikes().size(),
+                        comment.getArticle().getComments().size()
+                ))
+                .collect(Collectors.toList());
+    }
+
 }
