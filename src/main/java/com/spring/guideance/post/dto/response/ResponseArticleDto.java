518bc08a70198ecc4b5b59aab8303fbd69a40cab
CHAE <chaso1495@naver.com>
2023-08-15 13:17:25 +0900

    [fix] #13 ArticleDto 수정

--- src/main/java/com/spring/guideance/post/dto/response/ResponseArticleDto.java
+++ src/main/java/com/spring/guideance/post/dto/response/ResponseArticleDto.java
@@ -9,17 +9,21 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class ResponseArticleDto {
+    private Long articleId;
     private String title;
     private String contents;
     private String authorName;
+    private List<String> tags;
     private List<ResponseLikeDto> likes;
     private List<ResponseCommentDto> comments;
     private LocalDateTime createdAt;
 
-    public ResponseArticleDto(String title, String contents, String authorName, List<ResponseLikeDto> likes, List<ResponseCommentDto> comments, LocalDateTime createdAt) {
+    public ResponseArticleDto(Long articleId, String title, String contents, String authorName, List<String> tags, List<ResponseLikeDto> likes, List<ResponseCommentDto> comments, LocalDateTime createdAt) {
+        this.articleId = articleId;
         this.title = title;
         this.contents = contents;
         this.authorName = authorName;
+        this.tags = tags;
         this.likes = likes;
         this.comments = comments;
         this.createdAt = createdAt;
--- src/main/java/com/spring/guideance/post/dto/response/ResponseSimpleArticleDto.java
+++ src/main/java/com/spring/guideance/post/dto/response/ResponseSimpleArticleDto.java
@@ -6,13 +6,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ResponseSimpleArticleDto {
+    private Long articleId;
     private String title;
     private String contents;
     private String authorName;
     private int likesCount;
     private int commentCount;
 
-    public ResponseSimpleArticleDto(String title, String contents, String authorName, int likesCount, int commentCount) {
+    public ResponseSimpleArticleDto(Long articleId, String title, String contents, String authorName, int likesCount, int commentCount) {
+        this.articleId = articleId;
         this.title = title;
         this.contents = contents;
         this.authorName = authorName;
