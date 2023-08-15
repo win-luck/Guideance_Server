69fada2c48b3086eeb86bf4531919c6a45d5d201
CHAE <chaso1495@naver.com>
2023-08-15 15:19:28 +0900

    [exception] #15 GlobalExceptionHandler 작성

--- src/main/java/com/spring/guideance/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/guideance/util/exception/GlobalExceptionHandler.java
@@ -1,11 +1,37 @@
 package com.spring.guideance.util.exception;
 
+import com.spring.guideance.util.api.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ArticleException.class)
+    public ResponseEntity<ApiResponse<Void>> handleArticleException(ArticleException e) {
+        log.error("ArticleException: {}", e.getMessage());
+        return new ResponseEntity<>(ApiResponse.fail(e.getResponseCode()), e.getResponseCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ApiResponse<Void>> handleUserException(UserException e) {
+        log.error("UserException: {}", e.getMessage());
+        return new ResponseEntity<>(ApiResponse.fail(e.getResponseCode()), e.getResponseCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(NoticeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNoticeException(NoticeException e) {
+        log.error("NoticeException: {}", e.getMessage());
+        return new ResponseEntity<>(ApiResponse.fail(e.getResponseCode()), e.getResponseCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(TagException.class)
+    public ResponseEntity<ApiResponse<Void>> handleTagException(TagException e) {
+        log.error("TagException: {}", e.getMessage());
+        return new ResponseEntity<>(ApiResponse.fail(e.getResponseCode()), e.getResponseCode().getHttpStatus());
+    }
 
 }
