e0ab128836c76b549e67d0521168369e6bd1da77
CHAE <chaso1495@naver.com>
2023-08-19 00:43:20 +0900

    [chore] #25 댓글 삭제 요청 dto 수정

--- src/main/java/com/spring/guideance/post/dto/request/DeleteCommentDto.java
+++ src/main/java/com/spring/guideance/post/dto/request/DeleteCommentDto.java
@@ -7,10 +7,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DeleteCommentDto {
     private Long userId;
-    private Long commentId;
 
     public DeleteCommentDto(Long userId, Long commentId) {
         this.userId = userId;
-        this.commentId = commentId;
     }
 }
