0d376ee6edf2875c2138014351a0af6d93c252e9
CHAE <chaso1495@naver.com>
2023-08-20 14:38:51 +0900

    [chore] #34 TagRepository 누락된 Batchsize 적용

--- src/main/java/com/spring/guideance/tag/repository/TagRepository.java
+++ src/main/java/com/spring/guideance/tag/repository/TagRepository.java
@@ -14,6 +14,7 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
     Optional<Tag> findByTagName(String tagName);
 
+    @BatchSize(size = SizeType.SIZE.MEDIUM)
     Page<Tag> findByTagNameContaining(String tagName, Pageable pageable);
 
     boolean existsByTagName(String tagName);
