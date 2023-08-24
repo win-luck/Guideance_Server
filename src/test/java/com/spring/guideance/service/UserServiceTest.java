42ea5b8977219dca2e2b184923eb672513cbc719
CHAE <chaso1495@naver.com>
2023-08-24 20:31:58 +0900

    [test] #36 JUnit5 도입 및 UserServiceTest 재작성

--- src/test/java/com/spring/guideance/service/UserServiceTest.java
+++ src/test/java/com/spring/guideance/service/UserServiceTest.java
@@ -6,17 +6,15 @@ import com.spring.guideance.user.dto.response.ResponseUserDto;
 import com.spring.guideance.user.repository.UserRepository;
 import com.spring.guideance.user.service.UserService;
 import com.spring.guideance.util.exception.UserException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class UserServiceTest {
@@ -26,72 +24,42 @@ public class UserServiceTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    public void 회원가입(){
-        // given
+    @BeforeEach
+    public void init(){
+        userRepository.deleteAll();
         String name = "test";
-        String email = "";
-
-        // when
-        userService.createUser(new CreateUserDto(name, email));
-
-        // then
-        assertEquals(userRepository.findByEmail(email).orElse(null).getName(), name);
+        String keyCode = "a";
+        userService.createUser(new CreateUserDto(name, keyCode, null));
     }
 
     @Test
-    public void 중복이메일체크() throws UserException{
-        // given
-        String name = "test";
-        String email = "1";
-        userService.createUser(new CreateUserDto(name, email));
+    public void 회원가입(){
+        assertEquals( 1, userRepository.count());
+    }
 
-        // when
-        // 예외가 발생해야 한다.
-        assertThrows(UserException.class, () -> userService.createUser(new CreateUserDto(name, email)));
+    @Test
+    public void 중복키코드체크() throws UserException{
+        String name = "test";
+        String keyCode = "a";
+        assertThrows(UserException.class, () -> userService.createUser(new CreateUserDto(name, keyCode, null)));
     }
 
     @Test
     public void 로그인(){
-        // given
-        String name = "test";
-        String email = "123";
-        userService.createUser(new CreateUserDto(name, email));
-
-        // when
-        ResponseUserDto userDto = userService.login(email);
-
-        // then
-        assertEquals(userDto.getName(), name);
+        ResponseUserDto userDto = userService.login("a");
+        assertEquals("test", userDto.getName());
     }
 
     @Test
     public void 회원정보수정(){
-        // given
-        String name = "test";
-        String email = "1234";
-        Long id = userService.createUser(new CreateUserDto(name, email));
-
-        // when
-        String newName = "newTest";
-        userService.updateUser(new UpdateUserDto(id, newName), null);
-
-        // then
-        assertEquals(userRepository.findById(id).get().getName(), newName);
+        Long id = userRepository.findAll().get(0).getId();
+        userService.updateUser(new UpdateUserDto(id, "newName"), null);
+        assertEquals(userRepository.findById(id).get().getName(), "newName");
     }
 
     @Test
     public void 회원정보삭제(){
-        // given
-        String name = "test";
-        String email = "12345";
-        Long id = userService.createUser(new CreateUserDto(name, email));
-
-        // when
-        userService.deleteUser(id);
-
-        // then
-        // 예외가 발생해야 한다.
-        assertThrows(UserException.class, () -> userService.login(email));
+        userService.deleteUser(userRepository.findAll().get(0).getId());
+        assertEquals(0, userRepository.count());
     }
 }
