27017086a1db3cbf95c18edf82235c952a87b2f0
CHAE <chaso1495@naver.com>
2023-08-30 22:46:16 +0900

    [refactor] #44 카카오로그인 관련 코드 수정 및 일부 파일 삭제

--- src/main/java/com/spring/guideance/auth/controller/AuthController.java
+++ src/main/java/com/spring/guideance/auth/controller/AuthController.java
@@ -18,19 +18,16 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    // private final KakaoTokenJsonData kakaoTokenJsonData;
     private final KakaoUserInfo kakaoUserInfo;
 
     @PostMapping
     @ResponseBody
     public ApiResponse<ResponseUserDto> kakaoOauth(@RequestParam("token") String accessToken) {
-        // token는 클라이언트가 보낸 access_token이다.
         // 클라이언트는 서버로 accessToken을 보내고 서버는 accessToken을 이용하여 사용자의 정보를 가져온다.
         KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(accessToken);
         log.info("회원 정보 입니다.{}", userInfo);
-
         if (userService.isAlreadyUser(userInfo.getId().toString())) {
-            log.info("이미 가입된 회원입니다.");
+            log.info("이미 가입된 회원이므로 회원가입 없이 바로 로그인합니다.");
             return ApiResponse.success(userService.getUserByKeyCode(userInfo.getId().toString()), ResponseCode.USER_LOGINED.getMessage());
         } else {
             log.info("회원가입을 진행합니다.");
